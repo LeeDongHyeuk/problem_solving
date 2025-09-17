6e7ba75014a8f67a7f78801d9407eeb5de54889d
LeeDongHyeuk <80818718+LeeDongHyeuk@users.noreply.github.com>
2025-09-17 13:08:37 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numArr[10000];
+
+  int n, x;
+  cin >> n >> x;
+
+  for (int i = 0; i < n; i++) {
+    cin >> numArr[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (numArr[i] < x)
+      cout << numArr[i] << ' ';
+  }
+
+  cout << '\n';
+}
