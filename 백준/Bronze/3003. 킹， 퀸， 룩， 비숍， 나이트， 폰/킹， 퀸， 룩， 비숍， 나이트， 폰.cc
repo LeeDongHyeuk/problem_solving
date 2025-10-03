7d5f2d8c2d2413477158d53ad4a8c1df519f8c83
LeeDongHyeuk <80818718+LeeDongHyeuk@users.noreply.github.com>
2025-10-03 09:16:06 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int chess[6] = {1, 1, 2, 2, 2, 8};
+
+  for (int i = 0; i < 6; i++) {
+    int n;
+    cin >> n;
+
+    chess[i] -= n;
+  }
+
+  for (int i = 0; i < 6; i++) {
+    cout << chess[i] << " ";
+  }
+
+  cout << "\n";
+}
